9a4887960bc43a31de9506a9fd53e3caa4584eff
pramnegi <negi.1984@gmail.com>
2018-03-10 21:25:05 -0800

    Update HasSum.java

--- HasSum.java
+++ HasSum.java
@@ -1,31 +1,31 @@
 import java.util.Queue;
-class HasSum{
+class HasSum {
 
 	private BST bst;
 
-	HasSum(){
-		bst = new BST();
+	HasSum() {
+		this.bst = new BST();
 	}
 
-	private boolean checkSum(BNode node, int sum, int currentSum){
+	private boolean checkSum(BNode node, int sum) {
+		if(node == null)
+			return false;
 
-		if(node != null){
-			currentSum = currentSum + node.getValue();
-			if(sum == currentSum && node.getLeft() == null && node.getRight() == null)
-				return true;
-			return checkSum(node.getLeft() , sum , currentSum) || checkSum(node.getRight() , sum , currentSum);
+		if((sum == node.value) && node.getLeft() == null && node.getRight() == null) {
+			return true;
 		}
-		return false;
+
+		return checkSum(node.getLeft(), sum - node.value) || checkSum(node.getRight(), sum - node.value);
 	}
 
-	public void checkSum(int sum){
-		if (checkSum(this.bst.root , sum, 0)) 
+	public void checkSum(int sum) {
+		if (checkSum(this.bst.root, sum)) 
 			System.out.println("Sum exist!");
 		else
 			System.out.println("Sum not exist!");
 	}
 
-	public static void main(String[] argv){
+	public static void main(String[] argv) {
 		HasSum obj = new HasSum();
 		obj.bst.insert(70);
 		obj.bst.insert(90);
