de7e4c122dbaca88dc020ccff1cb8a309278ed43
ShikhaChauhan <shikhachauhan.nd@gmail.com>
2018-03-11 17:54:47 +0530

    Including Integer min and max value case.

--- IsBinarySearchTree.java
+++ IsBinarySearchTree.java
@@ -8,10 +8,10 @@ public class IsBinarySearchTree{
 	private boolean isBST(BNode node, int min, int max){
 		if(node == null)
 			return true;
-		if(node.getValue() <= min || max <= node.getValue())
+		if(node.getValue() < min || max < node.getValue())
 			return false;
 		//having assumption that the Right node of a given tree can have value greater than or equal to the parent node.
-		return isBST(node.getLeft(), min, node.getValue()) && isBST(node.getRight(), node.getValue()-1, max) ;					
+		return isBST(node.getLeft(), min, node.getValue()-1) && isBST(node.getRight(), node.getValue(), max) ;					
 	}
 
 	public void isBST(){
@@ -25,12 +25,14 @@ public class IsBinarySearchTree{
 	public static void main(String[] argv){
 		IsBinarySearchTree obj = new IsBinarySearchTree();
 		BNode node = new BNode(20);
+		//obj.bst.insert(Integer.MIN_VALUE);
+		obj.bst.insert(Integer.MAX_VALUE);
 		obj.bst.insert(70);
 		obj.bst.insert(90);
 		obj.bst.insert(30);
 		obj.bst.insert(30);
 		obj.bst.insert(20);
-		//obj.bst.root.getLeft().getLeft().getRight().setValue(30);
+		//obj.bst.root.getLeft().getLeft().setValue(30);
 		obj.bst.insert(80);
 		obj.bst.insert(10);
 		obj.bst.insert(50);
