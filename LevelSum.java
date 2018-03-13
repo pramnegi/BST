72a2cfac82aad6d63f77f6d794dbce34d1b1cb05
pramnegi <negi.1984@gmail.com>
2018-03-12 22:40:25 -0700

    [Done]

--- LevelSum.java
+++ LevelSum.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
-public class LevelSum{
+public class LevelSum {
 
-	private BST bst ; 
+	private BST bst; 
 
-	public LevelSum(){
+	public LevelSum() {
 		this.bst = new BST();
 	}
 
-	private void findEachLevelSum(BNode node, int level, Map<Integer,Integer> map){
+	private void findEachLevelSum(BNode node, int level, Map<Integer,Integer> map) {
 		if(node != null) {
 			if(map.containsKey(level))
 				map.put(level, map.get(level) + node.getValue());
@@ -20,17 +20,17 @@ public class LevelSum{
 	} 
 
 	//Wrapper function to call findEachLevelSum function.
-	public void findEachLevelSum(){
+	public void findEachLevelSum() {
 		Map<Integer, Integer> map = new HashMap<>();
 		findEachLevelSum(this.bst.root, 0, map);
 		if(map.size() == 0)
 			System.out.println("Tree is empty");
-		for(Map.Entry entry : map.entrySet()){
+		for(Map.Entry entry : map.entrySet()) {
 			System.out.println("Level " + entry.getKey() + " sum : " + entry.getValue());
 		}
 	}
 
-	public static void main(String[] argv){
+	public static void main(String[] argv) {
 		LevelSum obj = new LevelSum();
 		obj.bst.insert(7);
 		obj.bst.insert(9);
