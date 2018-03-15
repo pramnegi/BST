4478907bdc6e4fe2a5c2b73357df025f2074505c
ShikhaChauhan <shikhachauhan.nd@gmail.com>
2018-03-15 09:55:23 +0530

    Remove all nodes having value K

---
+++ RemoveAllK.java
@@ -0,0 +1,44 @@
+//Time Complexity : O(n)
+//Space Complexity : O(1)
+
+class RemoveAllK {
+
+	private LinkedList list;
+
+	RemoveAllK() {
+		this.list = new LinkedList();
+	}
+
+	private void removeK(int k) {
+		if(this.list.head == null)
+			return;
+		Node prev = null;
+		Node current = this.list.head;
+		while(current != null){
+			if(current.value == k){
+				if(prev == null)
+					this.list.head = current.next;
+				else
+					prev.next = current.next;
+				current = current.next;
+				continue;
+			}
+			prev = current;
+			current = current.next; 
+		}
+	}
+
+	public static void main(String[] argv) {
+		RemoveAllK obj = new RemoveAllK();
+		obj.list.insertBeg(40);
+		obj.list.insertBeg(50);
+		obj.list.insertBeg(20);
+		obj.list.insertBeg(30);
+		obj.list.insertBeg(10);
+		obj.list.insertBeg(60);
+		obj.list.insertBeg(20);
+		obj.list.printList();
+		obj.removeK(200);
+		obj.list.printList();
+	}
+}
