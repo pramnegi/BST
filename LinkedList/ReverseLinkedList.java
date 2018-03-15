a4effb21fadbb30445185538589817031702d9dd
ShikhaChauhan <shikhachauhan.nd@gmail.com>
2018-03-15 23:36:30 +0530

    Reverse given linked list

---
+++ LinkedList/ReverseLinkedList.java
@@ -0,0 +1,60 @@
+//Time Complexity : O(n)
+//Space Complexity of recursive : O(n)
+//Space Complexity of iterative : O(1)
+
+class ReverseLinkedList {
+
+	private LinkedList list;
+
+	ReverseLinkedList() {
+		this.list = new LinkedList();
+	}
+
+	private void iterativeReverseList(Node node) {
+		Node current = node;
+		Node prev = null;
+		Node next = null;
+		while(current != null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+		}
+		list.head = prev;
+	}
+
+	//Wrapper function to call reverseList.
+
+	public void iterativeReverseList() {
+		iterativeReverseList(this.list.head);
+	}
+
+	public void recursiveReverseList(Node node, Node prev){
+		if(node == null){
+			list.head = prev;
+			return;
+		}
+		Node next = node.next;
+		node.next = prev;
+		recursiveReverseList(next, node);
+	}
+
+	public void recursiveReverseList() {
+		recursiveReverseList(this.list.head, null);
+	}
+
+	public static void main(String[] argv) {
+		ReverseLinkedList obj = new ReverseLinkedList();
+		obj.list.insertBeg(40);
+		obj.list.insertBeg(50);
+		obj.list.insertBeg(20);
+		obj.list.insertBeg(30);
+		obj.list.insertBeg(10);
+		obj.list.insertBeg(60);
+		obj.list.printList();
+		obj.iterativeReverseList();
+		obj.list.printList();
+		obj.recursiveReverseList();
+		obj.list.printList();
+	}
+}
