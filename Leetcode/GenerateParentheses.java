b42743ef9d87b32d5c999dec1131c5e79fb087d9
pramnegi <negi.1984@gmail.com>
2018-03-25 13:14:30 -0700

    Update GenerateParentheses.java

--- Leetcode/GenerateParentheses.java
+++ Leetcode/GenerateParentheses.java
@@ -7,25 +7,23 @@ import java.util.ArrayList;
 
 class GenerateParentheses {
 
-	public static void generateParentheses(String str, int left, int right, ArrayList<String> list) {
-		if(left == 0 && right == 0) {
-			list.add(str);
-			return;
-		}
-
+	public static void generateParentheses(String str, int left, int right, ArrayList<String> result) {
 		if(left > right)
 			return;
 		
+		if(left == 0 && right == 0) {
+			result.add(str);
+			return;
+		}
+		
 		if(left > 0)
-			generateParentheses(str + "(", left - 1,right, list);
+			generateParentheses(str + "(", left - 1, right, result);
 		if(right > 0)
-			generateParentheses(str + ")", left, right - 1, list);
+			generateParentheses(str + ")", left, right - 1, result);
 	}
 
 	//Wrapper function to call generateParentheses.
-
 	public static ArrayList<String> generateParentheses(int num) {
-		
 		ArrayList<String> list = new ArrayList<>();
 		
 		if(num == 0)
